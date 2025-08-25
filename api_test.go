538b5ae5d732ad2f735224f94ebd08ad62e2dcf2
cliveyg <junkcat@gmail.com>
2025-08-25 13:45:36 +0100

    Add test for sql database errors

--- api_test.go
+++ api_test.go
@@ -1384,9 +1384,7 @@ func TestRowsError(t *testing.T) {
 	require.NoError(t, err)
 
 	// make the query return an error.
-	var rws *sqlmock.Rows
-	rws.AddRow(1)
-	mock.ExpectQuery("SELECT count(*) FROM .* WHERE .*").WillReturnRows(rws)
+	mock.ExpectQuery("SELECT count(*) FROM .* WHERE .*").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(42)
 	//mock.ExpectQuery("SELECT count(*) FROM .* WHERE .*").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
 	mock.ExpectQuery("SELECT .* FROM .* WHERE .* ORDER BY created desc LIMIT 3").WillReturnError(errors.New("forced error"))
 	a.DB = gormDB
