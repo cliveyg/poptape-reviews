e55e30574bce2452772990c08b31c4d9a8c2c883
cliveyg <junkcat@gmail.com>
2025-08-28 19:44:57 +0100

    Fix mocksql test errors

--- api_test.go
+++ api_test.go
@@ -1519,7 +1519,7 @@ func TestCreateReviewFailFetchItemBodyNotJson(t *testing.T) {
 
 func TestRowsError(t *testing.T) {
 	db, mock, err := sqlmock.New()
-	mock.ExpectClose()
+	//mock.ExpectClose()
 	require.NoError(t, err)
 	defer func(db *sql.DB) {
 		err := db.Close()
@@ -1565,7 +1565,7 @@ func TestRowsError(t *testing.T) {
 
 func TestMetaDataCountDBError(t *testing.T) {
 	db, mock, err := sqlmock.New()
-	mock.ExpectClose()
+	//mock.ExpectClose()
 	require.NoError(t, err)
 	defer func(db *sql.DB) {
 		err := db.Close()
